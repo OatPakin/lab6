d51a4b3a425ead634e8bade855be3b3155e8e1cb
Oat <kaoatheremoo@gmail.com_>
2023-12-01 16:25:18 +0700

    Filled out README

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x = 1;
+    int e = 0, o = 0;
+    while ( x != 0){
+        cout << "Enter an integer: ";
+        cin >> x;
+        if ( x%2 == 0 and x != 0 ){
+            e = e+1;
+        }else if( x%2 != 0){
+            o = o+1;
+        }
+    }
+    cout << "#Even numbers = "<< e << endl;
+    cout << "#Odd numbers = "<< o << endl;
     return 0;
 }
